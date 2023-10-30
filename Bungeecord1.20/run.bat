5ef69667b7ccb6b6a4d0e7da25c25c8a1906bbd8
Elián <elianroventini@outlook.com.br>
2023-10-30 19:41:25 -0300

    projeto minecraft

---
+++ Bungeecord1.20/run.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+java -Xms2G -Xmx2G -Dfile.encoding=UTF-8 -jar -jar BungeeCord.jar
+PAUSE
---
+++ Lobby1.20/run.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+java -Xms2G -Xmx2G -Dfile.encoding=UTF-8 -jar -jar Spigot1-20.jar
+PAUSE
